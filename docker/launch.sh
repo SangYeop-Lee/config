eb47d96f3a960876c489eb4d258a8f22567edf8f
Sangyeop Lee <ryan080198@gmail.com>
2023-05-17 19:29:32 +0900

    docker initialization to ./init.sh

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+MAINTAINER=sylee
+IMAGE_NAME=config_playground
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-IMAGE=sylee/config_playground
+IMAGE=$MAINTAINER/$IMAGE_NAME
 TAG=latest
-NAME=sylee_config_playground
-
+NAME="${MAINTAINER}_${IMAGE_NAME}"
 
 docker run -it \
     --net=host \
