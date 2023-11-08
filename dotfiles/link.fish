40f4ea4fa7697134bc0c3b55ccd57a88e22eacf9
Sangyeop Lee <ryan080198@gmail.com>
2023-11-08 16:39:13 +0900

    moved config files to hierarchical directories

--- dotfiles/link.fish
+++ dotfiles/link.fish
@@ -2,17 +2,17 @@
 
 cd (dirname (status --current-filename))
 
-source config.fish
+source ./fish/config.fish
 
-set -l H "$XDG_CONFIG_HOME"
+# set -l H "$XDG_CONFIG_HOME"
 
-for CONFIG_DIR in fish nvim nvim/lua git
-	mkdir -p $H/$CONFIG_DIR
+for CONFIG_DIR in fish nvim git
+	cp -r $CONFIG_DIR $XDG_CONFIG_HOME
 end
 
 # TODO add tmux,autojump
 # TODO maybe you need to create directory structures
-cp -f config.fish $H/fish/
-cp -f init.vim $H/nvim/
-cp -f init.lua $H/nvim/lua/
-cp -f config $H/git/config
+# cp -f config.fish $H/fish/
+# cp -f init.vim $H/nvim/
+# cp -f init.lua $H/nvim/lua/
+# cp -f config $H/git/config
