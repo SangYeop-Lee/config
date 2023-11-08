4ceb4c61a7a18bbeeac036db8d24b52e58367c2a
Sangyeop Lee <ryan080198@gmail.com>
2023-11-08 16:59:59 +0900

    add tmux conf

--- dotfiles/link.fish
+++ dotfiles/link.fish
@@ -4,15 +4,10 @@ cd (dirname (status --current-filename))
 
 source ./fish/config.fish
 
-# set -l H "$XDG_CONFIG_HOME"
-
-for CONFIG_DIR in fish nvim git
+for CONFIG_DIR in fish nvim git tmux
 	cp -r $CONFIG_DIR $XDG_CONFIG_HOME
 end
 
-# TODO add tmux,autojump
-# TODO maybe you need to create directory structures
-# cp -f config.fish $H/fish/
-# cp -f init.vim $H/nvim/
-# cp -f init.lua $H/nvim/lua/
-# cp -f config $H/git/config
+echo "# set shell
+set-option -g default-shell $(which fish)
+" >> $XDG_CONFIG_HOME/tmux/tmux.conf
