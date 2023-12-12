589f29a7d7375c95cff290c4b936d0856485782a
Sangyeop Lee <ryan080198@gmail.com>
2023-12-12 23:28:50 +0900

    add verilog language server

--- dotfiles/nvim/lua/init.lua
+++ dotfiles/nvim/lua/init.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
-require("mason-lspconfig").setup{
-	ensure_installed = { "clangd", "pyright", "dockerls", "taplo" }
+require("mason-lspconfig").setup {
+	ensure_installed = { "clangd", "pyright", "dockerls", "taplo", "verible" },
 }
 
 local lspconfig = require("lspconfig")
@@ -9,4 +9,5 @@ lspconfig.clangd.setup {}
 lspconfig.pyright.setup {}
 lspconfig.dockerls.setup {}
 lspconfig.taplo.setup {}
+lspconfig.verible.setup {}
 
