55278fc699f8dcbf2c418055f85ec3f05886f32c
Sangyeop Lee <ryan080198@gmail.com>
2023-12-13 20:13:14 +0900

    verible does not support mac. moving to svls

--- dotfiles/nvim/lua/init.lua
+++ dotfiles/nvim/lua/init.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = { "clangd", "pyright", "dockerls", "taplo", "verible" },
+	ensure_installed = { "clangd", "pyright", "dockerls", "taplo", "svls" },
 }
 
 local lspconfig = require("lspconfig")
@@ -9,5 +9,5 @@ lspconfig.clangd.setup {}
 lspconfig.pyright.setup {}
 lspconfig.dockerls.setup {}
 lspconfig.taplo.setup {}
-lspconfig.verible.setup {}
+lspconfig.svls.setup {}
 
