29afea8738e54f4a2d6117f2530ac9e7d3e7ad93
Sangyeop Lee <ryan080198@gmail.com>
2023-12-19 14:12:34 +0000

    fixed code to set node version 18

--- build/nvm.sh
+++ build/nvm.sh
@@ -3,5 +3,8 @@
 PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
 echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 18' >> ~/.bashrc
+if [[ ! $(node -v) =~ v18* ]]; then
+	nvm install 18
+	nvm alias default 18
+fi' >> ~/.bashrc
 
