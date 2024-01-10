41c4823aae09ca7d05409851d813eef694c26075
Sangyeop Lee <ryan080198@gmail.com>
2024-01-10 10:37:26 +0000

    fixed sudo operations

--- build/tmux.sh
+++ build/tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # apt install libevent ncurses
-apt install libevent-dev ncurses-dev build-essential bison pkg-config autoconf automake
+sudo apt install libevent-dev ncurses-dev build-essential bison pkg-config autoconf automake
 
 git clone https://github.com/tmux/tmux.git
 cd tmux
--- init.sh
+++ init.sh
@@ -12,15 +12,15 @@ export XDG_CACHE_HOME=$HOME/.cache" >> $HOME/.bashrc
 
 source $HOME/.bashrc
 
-apt update && apt install software-properties-common
+sudo apt update && sudo apt install software-properties-common
 
 # add ppas
-add-apt-repository ppa:fish-shell/release-3
-add-apt-repository ppa:neovim-ppa/stable
-add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:fish-shell/release-3
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:deadsnakes/ppa
 
 # install packages
-apt update && apt install -y \
+sudo apt update && sudo apt install -y \
 	curl \
 	wget \
 	htop \
