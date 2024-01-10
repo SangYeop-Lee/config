0256be32511e493bb936da8f35442e623a67ecde
Sangyeop Lee <ryan080198@gmail.com>
2024-01-10 10:45:08 +0000

    TODO: validate vimplug installation code

--- init.sh
+++ init.sh
@@ -34,6 +34,10 @@ sudo apt update && sudo apt install -y \
 	fish \
 	neovim
 
+# install vimplug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # link dotfiles
 ./dotfiles/link.fish
 
