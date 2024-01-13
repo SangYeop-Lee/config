32968e8432e3727ed434c70bcbd2b7a15227e1d7
Sangyeop Lee <ryan080198@gmail.com>
2024-01-14 02:07:05 +0900

    fixed nvm installation script

--- build/nvm.sh
+++ build/nvm.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-XDG_CONFIG_HOME="$HOME/.config"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | PROFILE=~/.bashrc bash
 
-PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
-
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
+echo '
 if [[ ! $(node -v) =~ ^v18 ]]; then
 	nvm install 18
 	nvm alias default 18
