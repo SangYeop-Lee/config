4da95d73c2638be9b3ea84c154ad16eb7b994067
Sangyeop Lee <ryan080198@gmail.com>
2024-01-27 04:09:08 +0900

    updated readme && made driver parameter to null

--- docker/gpu-docker/README.md
+++ docker/gpu-docker/README.md
@@ -1,5 +1,5 @@
 # Prerequisite
 - docker installation
 - from `./settings` install gpu drivers, and nvidia-container-toolkit
-- test toolkit installation with the command below
+- test toolkit installation with the command below\
 ```sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi```
--- docker/gpu-docker/settings/nvidia-driver-config.sh
+++ docker/gpu-docker/settings/nvidia-driver-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# check your appropriate driver version here. https://www.nvidia.com/Download/index.aspx?lang=en-us
 BASE_URL=https://us.download.nvidia.com/tesla
-DRIVER_VERSION=535.154.05
+DRIVER_VERSION=
 
 curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
 
