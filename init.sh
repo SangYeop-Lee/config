490a064a10fc04ca6370fca7279dbc247d1a579e
Sangyeop Lee <ryan080198@gmail.com>
2024-02-05 13:28:34 +0900

    differentiated sudo comment base on user

--- init.sh
+++ init.sh
@@ -14,25 +14,48 @@ source $HOME/.bashrc
 
 sudo apt update && sudo apt install software-properties-common
 
-# add ppas
-sudo add-apt-repository ppa:fish-shell/release-3
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo add-apt-repository ppa:deadsnakes/ppa
+if [ "$EUID" -eq 0 ]
+then
+	apt update && apt install software-properties-common
+	
+	# add ppas
+	add-apt-repository ppa:fish-shell/release-3
+	add-apt-repository ppa:neovim-ppa/stable
+	add-apt-repository ppa:deadsnakes/ppa
+	
+	# install packages
+	apt update && apt install -y \
+		curl \
+		wget \
+		htop \
+		unzip \
+		fish \
+		neovim
+else
+	sudo apt update && sudo apt install software-properties-common
+	
+	# add ppas
+	sudo add-apt-repository ppa:fish-shell/release-3
+	sudo add-apt-repository ppa:neovim-ppa/stable
+	sudo add-apt-repository ppa:deadsnakes/ppa
+	
+	# install packages
+	sudo apt update && sudo apt install -y \
+		curl \
+		wget \
+		htop \
+		unzip \
+		fish \
+		neovim
+fi
 
-# install packages
-sudo apt update && sudo apt install -y \
-	curl \
-	wget \
-	htop \
-	ripgrep \
-	autojump \
-	python3 \
-	python3-venv \
-	python3-pip \
-	unzip \
-	npm \
-	fish \
-	neovim
+
+#	npm \
+#	ripgrep \
+#	autojump \
+# 	python3 \
+# 	python3-venv \
+# 	python3-pip \
 
 # install vimplug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
