eaa3321c5a7ea105848733a6d8212e5fe68ffda7
Sangyeop Lee <ryan080198@gmail.com>
2024-04-24 04:51:27 +0000

    do not update as default

--- init.sh
+++ init.sh
@@ -2,6 +2,22 @@
 
 set -xe
 
+# Default value for the flag
+UPDATE_FLAG=false
+
+# Parse command-line options
+while getopts ":u" opt; do
+  case ${opt} in
+    u )
+      UPDATE_FLAG=true
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
 cd $(dirname ${BASH_SOURCE[0]})
 
 echo "
@@ -14,7 +30,10 @@ source $HOME/.bashrc
 
 if [ "$EUID" -eq 0 ]
 then
-	apt update && apt install software-properties-common
+	if [ "$UPDATE_FLAG" = "true" ]; then
+		apt update
+	fi
+	apt install software-properties-common build-essential
 	
 	# add ppas
 	add-apt-repository ppa:neovim-ppa/stable
@@ -28,7 +47,10 @@ then
 		unzip \
 		neovim
 else
-	sudo apt update && sudo apt install software-properties-common
+	if [ "$UPDATE_FLAG" = "true" ]; then
+		sudo apt update
+	fi
+	sudo apt install software-properties-common build-essential
 	
 	# add ppas
 	sudo add-apt-repository ppa:neovim-ppa/stable
@@ -42,14 +64,6 @@ else
 		unzip \
 		neovim
 fi
-
-
-#	npm \
-#	ripgrep \
-#	autojump \
-# 	python3 \
-# 	python3-venv \
-# 	python3-pip \
 
 # install vimplug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
