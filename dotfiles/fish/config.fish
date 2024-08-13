0ca9cd930ed75dcd531aa2e09af028dc2d405c9f
SangYeop-Lee <ryan080198@gmail.com>
2024-08-13 15:47:05 +0900

    removed prompt styling

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -71,21 +71,22 @@ function fish_user_key_bindings
     end
 end
 
-function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s ' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
-end
+# remove fish_prompt func
+# function fish_prompt
+# 	set_color brblack
+# 	echo -n "["(date "+%H:%M")"] "
+# 	set_color blue
+# 	echo -n (hostname)
+# 	if [ $PWD != $HOME ]
+# 		set_color brblack
+# 		echo -n ':'
+# 		set_color yellow
+# 		echo -n (basename $PWD)
+# 	end
+# 	set_color green
+# 	printf '%s ' (__fish_git_prompt)
+# 	set_color red
+# 	echo -n '| '
+# 	set_color normal
+# end
 
