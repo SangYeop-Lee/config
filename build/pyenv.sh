01e6dca3e95763e777f57785a87d43c9222da87a
Sangyeop Lee <ryan080198@gmail.com>
2024-08-14 23:30:52 +0900

    fixed pyenv installation

--- build/pyenv.sh
+++ build/pyenv.sh
@@ -2,5 +2,5 @@
 curl https://pyenv.run | bash
 
 fish -c 'set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv'
-fish -c 'echo fish_add_path $PYENV_ROOT/bin'
+fish -c 'fish_add_path $PYENV_ROOT/bin'
 echo 'pyenv init - | source' >> $XDG_CONFIG_HOME/fish/config.fish
