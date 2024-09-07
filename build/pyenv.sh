a6faf1dbb93eb5f00291d5467bf4c5964e8af25c
SangYeop Lee <ryan080198@gmail.com>
2024-09-08 03:57:33 +0900

    it adds xdg directory when installing pyenv

--- build/pyenv.sh
+++ build/pyenv.sh
@@ -3,9 +3,10 @@
 # install python dependencies
 sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev libncurses-dev tk-dev
 
+source ~/.bashrc
 export PYENV_ROOT=$XDG_DATA_HOME/pyenv
 
 curl https://pyenv.run | bash
 
-fish -c 'set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv'
-fish -c 'fish_add_path $PYENV_ROOT/bin'
+echo fish -c "set -Ux PYENV_ROOT $PYENV_ROOT"
+echo fish -c "fish_add_path $PYENV_ROOT/bin"
