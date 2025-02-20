a8405c43e4973b147925b110939cbbf20c953c32
Sangyeop Lee <ryan080198@gmail.com>
2025-02-20 06:23:47 +0000

    add git update & fixed launch script mounts

--- docker/ubuntu-server/launch.sh
+++ docker/ubuntu-server/launch.sh
@@ -8,13 +8,11 @@ TAG=latest
 NAME="${MAINTAINER}_${IMAGE_NAME}"
 
 docker run -it \
-    --net=host \
+    --net host \
     --gpus all \
-    --shm-size=128g \
+    --shm-size=200g \
     --name ${NAME} \
-    -v /home/${USER}/workspace:/home/${USER}/workspace \
-    -v /home/${USER}/.cache:/home/${USER}/.cache \
-    -v /home/${USER}/.ssh:/home/${USER}/.ssh \
-    -v /home/${USER}/data:/data \
-    -v /mnt:/mnt \
+    -v ${HOME}/workspace:${HOME}/workspace \
+    -v ${HOME}/.cache:${HOME}/.cache \
+    -v ${HOME}/.ssh:${HOME}/.ssh \
     ${IMAGE}:${TAG} bash
